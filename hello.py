572109985ea891996a7613f06bc20cfbe634bb01
Kevin <neoglitch822@yahoo.com>
2023-10-09 14:36:59 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-def hello(to):
+def main():
+    name = input("What is your name? ")
+    hello(name)
+
+def hello(to="world"): # the to="" means a default in case the programmer doesn't call hello with an argument
     print("hello,", to)
 
-
-name = input("What is your name? ")
-hello(name)
-
+main()
 
 # Use plus operator to concat 2 srtings/arguments, any more and use commas or algorithms.
 # Passing multiple args to "print" will add a space after the string. Do "hello," and not "hello, " or use end="" to override the...
