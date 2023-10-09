663d11b6f6ad6adca0fd8aaaf52f544397c6200e
Kevin <neoglitch822@yahoo.com>
2023-10-09 14:09:41 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 x = float(input("What is x? "))
 y = float(input("What is y? "))
 
+# Round rounds up
 z = round(x + y)
 
-print(z)
+print(f"{z:,}") # Colon-Comma in a format string tells the reader to add a thousands separater
