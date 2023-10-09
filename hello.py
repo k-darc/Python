84ec7240234d776d8f21b027e2052a2110ee0c04
Kevin <neoglitch822@yahoo.com>
2023-10-09 13:23:56 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 # First File - Check GH for test and this file
 name = input("What is your name? ")
-print("hello," + name)
+print("Hello, ", end="" + name)
 
-# Use plus operator to concat strings 2 srtings, any more and use commas or algorithms.
-# Passing multiple args to "print" will add a space after the string. Do "hello," and not "hello, "
+# Use plus operator to concat 2 srtings/arguments, any more and use commas or algorithms.
+# Passing multiple args to "print" will add a space after the string. Do "hello," and not "hello, or use end="" to override the default...
+# value for print.
 # Inside parentheses is read first.
