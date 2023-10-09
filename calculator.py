ea4319547882215fd2612fc65a9c9e1b44ec7b03
Kevin <neoglitch822@yahoo.com>
2023-10-09 14:15:01 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,5 +3,7 @@ y = float(input("What is y? "))
 
 z = round(x / y, 2)
 
-print(z)
-# Colon-Comma "print({x:,})" in a format string tells the reader to add a separater. Floats can represent numbers accurately forever, they have to round as there is not enough memory in a PC
+print(f"{z:.2f}")
+# Colon-Comma "print({z:,})" in a format string tells the reader to add a separater. Floats can represent numbers accurately...
+#   forever, they have to round as there is not enough memory in a PC.
+# "print({"z:.2f"})" specify an f strings, how many digits you want to print.
