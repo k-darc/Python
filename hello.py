1a764718edeee9c08029ab22fd010e6e03557e6b
Kevin <neoglitch822@yahoo.com>
2023-10-09 13:31:43 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 # First File - Check GH for test and this file
 name = input("What is your name? ")
-print("Hello, ", end="" + name)
+print(f"Hello, {name}")
+
+print(f"Hello, \"{name}\"") # These back slashes show how to use quotes in a format string.
 
 # Use plus operator to concat 2 srtings/arguments, any more and use commas or algorithms.
-# Passing multiple args to "print" will add a space after the string. Do "hello," and not "hello, or use end="" to override the default...
+# Passing multiple args to "print" will add a space after the string. Do "hello," and not "hello, " or use end="" to override the default...
 # value for print.
 # Inside parentheses is read first.
