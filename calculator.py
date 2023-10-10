e6ba031f0a9ccccd08752a05651d7a89bb3f9176
Kevin <neoglitch822@yahoo.com>
2023-10-10 12:34:17 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,3 +10,4 @@ main()
 # Colon-Comma "print({z:,})" in a format string tells the reader to add a separater. Floats can represent numbers accurately...
 #   forever, they have to round as there is not enough memory in a PC.
 # "print({"z:.2f"})" specify an f strings, how many digits you want to print.
+
