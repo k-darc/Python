a3f977e7c833078bc22b892ff3d4f2d28f89a13f
Kevin <neoglitch822@yahoo.com>
2023-10-10 13:30:14 -0400

    Create house.py

---
+++ house.py
@@ -0,0 +1,12 @@
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Heromine":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherine")
+else:
+    print("Who?")
