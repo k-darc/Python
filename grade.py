ce938202b77cf771dbd219c0e8aed8c85ec03870
Kevin <neoglitch822@yahoo.com>
2023-10-10 13:13:19 -0400

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,12 +1,12 @@
 score = int(input("Your Score: "))
 
-if score <= 90:
+if score >= 90:
     print("Grade A: ")
-elif score <= 80:
+elif score >= 80:
     print("Grade B: ")
-elif score <= 70:
+elif score >= 70:
     print("Grade C: ")
-elif score <= 60:
+elif score >= 60:
     print("Grade D: ")
 else:
     print("Grade F:")
