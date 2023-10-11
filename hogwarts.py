b6ce74b5336b2b967a774f05006b407f8c0d709e
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:58:53 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,7 +1,10 @@
-students = ["Hermione", "Harry", "Ron"]
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+    }
 
-for i in range(len(students)):
-    print(i + 1, students[i])
-
+print(students["Hermione"])
 
 # lists - make the decision to name "student" and not name it "_" as it is too cryptic. You should call your varible what they are.
