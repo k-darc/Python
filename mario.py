6ba7a3162874cd653d5a4b1dbe505a92ff6a98a3
Kevin <neoglitch822@yahoo.com>
2023-10-11 15:05:30 -0400

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,7 +1,17 @@
 def main():
-    print_column(3)
+    print_square(3)
 
-def print_column(height):
-    print("#\n" * height, end="")
+
+def print_square(size):
+
+    # For each row in square
+    for i in range(size):
+
+        # For each brick in row
+        for j in range(size):
+
+            # Print brick
+            print("#", end="")
+        print()
 
 main()
