19d83759c91b37427c7f0fd6c16629896dad3f15
Kevin <neoglitch822@yahoo.com>
2023-10-11 14:56:13 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,11 +1,11 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-    }
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["house"],student["patronus"], sep=", ")
 
 # lists - make the decision to name "student" and not name it "_" as it is too cryptic. You should call your varible what they are.
