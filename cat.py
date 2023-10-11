f41dae19afb2feb66ae4f168d8468036c2067dc6
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:36:22 -0400

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,4 +1,3 @@
-for i in [0, 1, 2]:
-    print("meow")
+print(f"meow\n" * 3, end="") # print alread puts the cursor in a new line, \n adds the next meow on a new line and end="" delete the blank new line that print outputs.
 
 # using loops... and this was a good way to learn about Ctrl + C to cancel the runtime
