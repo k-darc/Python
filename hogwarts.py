2b1302cca02f7a1e91ba37e9e1e993030f5ae608
Kevin <neoglitch822@yahoo.com>
2023-10-11 14:50:42 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -5,6 +5,7 @@ students = {
     "Draco": "Slytherin"
     }
 
-print(students["Hermione"])
+for student in students:
+    print(student, students[student], sep=", ")
 
 # lists - make the decision to name "student" and not name it "_" as it is too cryptic. You should call your varible what they are.
