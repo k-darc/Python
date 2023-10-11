ca39163d44d7e6416a7d62f9f8c345e31bc5447f
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:49:08 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -4,4 +4,4 @@ for student in students:
     print(student)
 
 
-# lists
+# lists - make the decision to name "student" and not name it "_" as it is too cryptic. You should call your varible what they are.
