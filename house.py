763c6f17f909c6dc1b5063a2831150d29700aa00
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:21:21 -0400

    Update house.py

--- house.py
+++ house.py
@@ -1,7 +1,7 @@
 name = input("What's your name? ")
 
 match name:
-    case "Harry" | "Hermione" | "Ron":
+    case "Harry" | "Hermione" | "Ron": # the bar | acts as an or
         print("Gryffindor")
     case "Draco":
         print("Slytherine")
