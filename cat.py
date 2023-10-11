2b3fbeebf8de20260d8c1b2b9d71fe941c083ab5
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:24:06 -0400

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,5 +1,5 @@
-print("Meow")
-print("Meow")
-print("Meow")
+i = 3
+while i != 0:
+    print("Meow")
 
-# testing recursion
+# using a while loop... and this was a good way to learn about Ctrl + C to cancel the runtime
