be8989194cce93b7ee688480aa3fb7f2e1a7093f
Kevin <neoglitch822@yahoo.com>
2023-10-11 13:27:22 -0400

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,6 +1,6 @@
-i = 3
-while i != 0:
+i = 0
+while i < 3:
     print("Meow")
-    i = i - 1
+    i += 1
 
 # using a while loop... and this was a good way to learn about Ctrl + C to cancel the runtime
