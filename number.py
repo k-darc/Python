2e2fe89a2a0c3a001d63fdc6323c204a0fbc7c14
Kevin <neoglitch822@yahoo.com>
2023-10-13 16:24:51 -0400

    Update number.py
    added error catch with loop to keep asking user for integer, instead of exiting program

--- number.py
+++ number.py
@@ -1,8 +1,12 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("X is not an integer")
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("X is not an integer")
+    else:
+        break
 
-print(f"x is {x}")
+print(f"X is {x}")
 
-# Order of Operations error - the input is passing the string to the int function as its argument, the int function is the error
+# Order of Operations error - the input is passing the string to the int function as its argument, the int function is the error.
+# If you TRY this code, and there is a ValueError, it'll now show the "not an integer"
