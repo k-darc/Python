4c155595c3b89e9f3cf431281edcfaef7a9d635e
Kevin <neoglitch822@yahoo.com>
2023-10-13 16:17:09 -0400

    Update number.py

--- number.py
+++ number.py
@@ -1,5 +1,8 @@
 try:
     x = int(input("What's x? "))
-    print(f"x is {x}")
 except ValueError:
     print("X is not an integer")
+
+print(f"x is {x}")
+
+# Order of Operations error - the input is passing the string to the int function as its argument, the int function is the error
