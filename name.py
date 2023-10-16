88cbce594e6380af3cdcad39b1e7c25966778b07
Kevin <neoglitch822@yahoo.com>
2023-10-16 16:06:42 -0400

    Update name.py

--- name.py
+++ name.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     print("hello, my name is", arg)
 
 # run python name.py <text>. example: python name.py Kevin
