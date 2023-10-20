3a393aa5f18208123a56d79d0811e3b902e31bf9
Kevin <neoglitch822@yahoo.com>
2023-10-20 14:16:10 -0400

    compact, but we if we need to make changes to the data  we cant iterate over it

--- names.py
+++ names.py
@@ -1,7 +1,11 @@
+names = []
 
 with open("names.txt") as file:
-    for line in sorted(file):
-        print("hello,", line.rstrip())
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
 
 # each name entered on run will exist in names.txt. "Code names.txt" to see what you have added "rm names.txt" to delete the file.
 # "file = open("names.txt", "w")" the W means write, but it opens the file and recreates it with brand new content, and does not append.
