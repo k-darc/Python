b2a68d20dde0c383ffc59672048cb3c7f3f88d69
Kevin <neoglitch822@yahoo.com>
2023-10-20 14:09:29 -0400

    Update names.py

--- names.py
+++ names.py
@@ -1,8 +1,6 @@
 with open("names.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    print("hello,", line.rstrip())
+    for line in file:
+        print("hello,", line.rstrip())
 
 
 
