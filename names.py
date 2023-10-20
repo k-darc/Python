ec516ed177bd293f12d810d072815abe85f01fab
Kevin <neoglitch822@yahoo.com>
2023-10-20 14:05:29 -0400

    appending to lists

--- names.py
+++ names.py
@@ -1,4 +1,10 @@
-names = []
+name = input("What's your name? ")
 
-for _ in range(3):
-    names.append(input("What's your name? "))
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+
+
+
+# each name entered on run will exist in names.txt. "Code names.txt" to see what you have added "rm names.txt" to delete the file.
+# "file = open("names.txt", "w")" the W means write, but it opens the file and recreates it with brand new content, and does not append.
+#    use A to rewrite and append.
