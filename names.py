d080266cd62c16b64a268c408ff18fa09b62beb1
Kevin <neoglitch822@yahoo.com>
2023-10-20 14:14:29 -0400

    Update names.py

--- names.py
+++ names.py
@@ -1,9 +1,14 @@
-with open("names.txt", "r") as file:
+names = []
+
+with open("names.txt") as file:
     for line in file:
-        print("hello,", line.rstrip())
+        names.append(line.rstrip())
 
-
+for name in sorted(names):
+    print(f"hello, {name}")
 
 # each name entered on run will exist in names.txt. "Code names.txt" to see what you have added "rm names.txt" to delete the file.
 # "file = open("names.txt", "w")" the W means write, but it opens the file and recreates it with brand new content, and does not append.
 #    use A to rewrite and append.
+
+# with open("names.txt") you do not need the "r" here, as when opening a file to read it, R is the default to read and not write.
