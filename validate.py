ab50f1c7f50cd90c0fdc536ac880a39c40a13a4e
Kevin <neoglitch822@yahoo.com>
2023-10-23 11:48:23 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What your email? ").strip()
 
-if re.search(r"^[^@]+@[^@]+\.edu$", email):
+if re.search(r"^[a-zA-Z0-9_]+@[^@]+\.edu$", email):
     print("Valid")
 else:
     print("INVALID!")
