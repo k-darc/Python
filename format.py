fb6bd6af26a22d7e13827f9cd88eb743e5705353
Kevin <neoglitch822@yahoo.com>
2023-10-23 14:36:56 -0400

    Update format.py

--- format.py
+++ format.py
@@ -1,7 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name) # the parenthesis are "captured" and then returned as the last name and first name
+matches = re.search(r"^(.+), *(.+)$", name) # the parenthesis are "captured" and then returned as the last name and first name
 if matches:
     name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
