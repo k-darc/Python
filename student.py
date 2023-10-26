ab50e210fc61702bbf67d4c528dd0561e4219fb9
Kevin <neoglitch822@yahoo.com>
2023-10-26 11:25:46 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,37 +1,25 @@
 class Student:
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
-        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "A Stag appears!"
-            case "Otter":
-                return "An Otter appears!"
-            case "Jack Russell terrier":
-                return "A Jack Russell appears!"
-            case _:
-                return "Nothing extraordinary happens...."
-
 def main():
     student = get_student()
-    print("Expecto Patronum!")
-    print(student.charm())
+    student.house = "Number Four, Privet Drive"
+    print(student)
+
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
