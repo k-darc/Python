a3e8f5a28c4f8db3d42339d97cf883d3658d7fce
Kevin <neoglitch822@yahoo.com>
2023-10-30 11:59:16 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -2,8 +2,15 @@ balance = 0
 
 def main():
     print("Balance: ", balance)
+    deposit(100)
+    withdraw(50)
+    print("New Balance:", balance)
 
+def deposit(n):
+    balance += n
 
+def withdraw (n):
+    balance -= n
 
 if __name__ == "__main__":
     main()
