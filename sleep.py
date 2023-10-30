f9760f1891d7b6bb87e56cfd3578da7b9f50f7a4
Kevin <neoglitch822@yahoo.com>
2023-10-30 14:14:37 -0400

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,3 +1,7 @@
-n = int(input("What's n? "))
-for i in range(n):
-    print("Sheep" * i)
+def main():
+    n = int(input("What's n? "))
+    for i in range(n):
+        print("Sheep" * i)
+
+if __name__ == "__main__":
+    main()
