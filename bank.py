cff2fae349c98b5b4e1aa7923412a19d8fda6c27
Kevin <neoglitch822@yahoo.com>
2023-10-30 12:18:02 -0400

    using OOP

--- bank.py
+++ bank.py
@@ -1,18 +1,7 @@
-balance = 0
+class Account:
+    def __init__(self):
+        self._balance = 0
 
-def main():
-    print("Balance: ", balance)
-    deposit(100)
-    withdraw(50)
-    print("New Balance:", balance)
-
-def deposit(n):
-    global balance
-    balance += n
-
-def withdraw (n):
-    global balance
-    balance -= n
-
-if __name__ == "__main__":
-    main()
+    @property
+    def balance(self):
+        return self._balance
