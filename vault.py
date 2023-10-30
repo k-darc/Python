0c97d3e954cfeca81ae1301d725e086d669fcfd8
Kevin <neoglitch822@yahoo.com>
2023-10-30 11:01:25 -0400

    operating overloading

--- vault.py
+++ vault.py
@@ -13,9 +13,5 @@ print(potter)
 weasley = Vault(25, 50, 100)
 print(weasley)
 
-galleons = potter.galleons + weasley.galleons
-sickles = potter.sickles + weasley.sickles
-knuts = potter.knuts + weasley.knuts
-
-total = Vault(galleons, sickles, knuts)
+total = potter + weasley
 print(total)
