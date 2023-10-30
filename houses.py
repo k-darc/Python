cb49cc31921ef865282f4794dbe425c104741b15
Kevin <neoglitch822@yahoo.com>
2023-10-30 11:50:17 -0400

    Create houses.py

---
+++ houses.py
@@ -0,0 +1,11 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"}
+]
+
+houses =[]
+for student in students:
+    if student["house"]
