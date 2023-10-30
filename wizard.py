26257f395a1583bccd941414757e0af851578889
Kevin <neoglitch822@yahoo.com>
2023-10-29 20:24:12 -0400

    Create wizard.py

---
+++ wizard.py
@@ -0,0 +1,13 @@
+class Student:
+    def __int__(self, anme, house):
+        self.name = name
+        self.house = house
+        
+        ...
+
+class Professor:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+
+        ...
