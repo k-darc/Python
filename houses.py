7c54f23a0a58068e3160ecb3845baf57d3561377
Kevin <neoglitch822@yahoo.com>
2023-10-30 11:50:57 -0400

    Update houses.py

--- houses.py
+++ houses.py
@@ -8,4 +8,8 @@ students = [
 
 houses =[]
 for student in students:
-    if student["house"]
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+for house in sorted(houses):
+    print(house)
